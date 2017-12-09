adb205c8ff49101025e20abe600bb18896cb3ca1
NikolayNN <kolya.by@mail.ru>
2017-12-09 11:21:10 +0300

    added sample how to delete records from db

---
+++ src/main/java/com/my/tutorial/hibernate/jdbc/samples/DeleteStudentDemo.java
@@ -0,0 +1,37 @@
+package com.my.tutorial.hibernate.jdbc.samples;
+
+import com.my.tutorial.hibernate.jdbc.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            int studentId = 1;
+            System.out.println("Getting student with id: " + studentId);
+            Student student = session.get(Student.class, studentId);
+            session.delete(student);
+
+            //another way
+
+            session.createQuery("delete from Student where id=2").executeUpdate();
+
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+        }
+    }
+}
