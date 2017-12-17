095bc5bbe3fd2e2b16ba2bc3a1ae4f19bcc3d801
NikolayNN <kolya.by@mail.ru>
2017-12-17 11:14:01 +0300

    bi directional oneToOne mapping cascade delete

---
+++ src/main/java/mapping/samples/biDirectionalOneToOneDeleteDemo.java
@@ -0,0 +1,42 @@
+package mapping.samples;
+
+import mapping.entity.Instructor;
+import mapping.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class biDirectionalOneToOneDeleteDemo {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            int theId = 3;
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, theId);
+
+            if (instructorDetail != null) {
+                session.delete(instructorDetail);
+                System.out.println(instructorDetail.getInstructor());
+            }
+
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+}
--- src/main/java/mapping/samples/biDirectionalOneToOneGetDemo.java
+++ src/main/java/mapping/samples/biDirectionalOneToOneGetDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class biDirectionalOneToOneDemo {
+public class biDirectionalOneToOneGetDemo {
 
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration()
@@ -17,19 +17,19 @@ public class biDirectionalOneToOneDemo {
 
         Session session = sessionFactory.getCurrentSession();
 
-        try{
+        try {
 
-        session.beginTransaction();
+            session.beginTransaction();
 
-        int theId = 1999;
-        InstructorDetail instructorDetail = session.get(InstructorDetail.class, theId);
+            int theId = 1999;
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, theId);
 
             System.out.println(instructorDetail.getInstructor());
+            session.getTransaction().commit();
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             session.close();
             sessionFactory.close();
         }
